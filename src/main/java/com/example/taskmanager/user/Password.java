dab9874fd92173d5295c713fef891b25267d6cb8
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-07-02 20:35:17 +0300

    Add constraint to password decorator

--- src/main/java/com/example/taskmanager/user/Password.java
+++ src/main/java/com/example/taskmanager/user/Password.java
@@ -1,20 +1,23 @@
 package com.example.taskmanager.user;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 
 @NotNull
 @NotBlank
 @Size(min = 6)
+@Documented
+@Constraint(validatedBy = {})
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Password {
-
+    String message() default "The password does not comply with the requirements.";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
 }
